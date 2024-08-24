ad2b9a1661e49a5002f69f9210fb5734f283e53b
AmirDapl <Amir@daplcorp.com>
2024-08-24 15:10:59 -0500

    Update config.js
    Added ENV pointers for config items

--- src/util/config.js
+++ src/util/config.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:3001/api/v0/'
-const SERVER_URL = 'http://localhost:3001'
-const KADENA_EXPLORER = 'https://explorer.chainweb.com/mainnet/tx'
+const API_URL = import.meta.env.API || 'http://localhost:3001/api/v0/'
+const SERVER_URL = import.meta.env.SERVER || 'http://localhost:3001'
+const KADENA_EXPLORER = import.meta.env.API || 'https://explorer.chainweb.com/mainnet/tx'
 
 export { API_URL, KADENA_EXPLORER, SERVER_URL }
